132074424fdaa2e4d7bc19cae69045c260c2dc29
Ayoub <elbouzidiayoub.solicode@gmail.com>
2025-07-17 16:12:20 +0200

    stack

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+//the stack and the heap
+class Program
+{
+    //the stack and the heap
+    //the stack is for value types, the heap is for reference types
+    //the stack is faster than the heap
+    //the stack is limited in size, the heap is not
+    //the stack is used for method calls, the heap is used for objects
+    static void Main(string[] args)
+    {
+        int result = Factorial(5);
+        Console.WriteLine($"Factorial of 5 is: {result}");
+    }
+    static int Factorial(int x)
+    {
+        if (x == 0) return 1;
+        return x * Factorial(x - 1);
+    }
+}
